9d4fc10a24d654540568d7a8b5e47104c02b0463
DrGARCIA31 <99087026+DrGARCIA31@users.noreply.github.com>
2022-03-19 11:43:30 -0500

    TestPass

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,17 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PretestApplicationTests {
 
     @Test
     void contextLoads() {
+        String dbPassword = "";
+        String password = "";
+        UserUtils userUtils = new UserUtils();
+        assertTrue(UserUtils.isPasswordEqual(dbPassword,password));
+
     }
 
 }
